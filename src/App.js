b844965ea0f55335c100866e730f73afe82946e8
Prajwal <prajju.18gryphon@gmail.com>
2025-05-08 12:06:06 +0530

    Text form Uppercase

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import Navbar from './Components/Navbar';
+import Textform from './Components/Textform';
 
 function App() {
   return (
     <>         
         <Navbar title="TextUtils" about="About"/>
+        <div className='container my-3'> 
+        <Textform heading = "Enter The Text To Analyze Below"/>
+        </div>
     </>
   );
 }
