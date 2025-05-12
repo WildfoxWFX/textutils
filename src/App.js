62964d01a5d59d350f3b71a11dbbb61d541eb470
Prajwal <prajju.18gryphon@gmail.com>
2025-05-12 12:20:27 +0530

    About Page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import './App.css';
 import Navbar from './Components/Navbar';
 import Textform from './Components/Textform';
+import About from './Components/About';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <>         
-        <Navbar title="TextUtils" about="About"/>
-        <div className='container my-3'> 
-        <Textform heading = "Enter The Text To Analyze Below"/>
-        </div>
-    </>
+    <Router>
+      <Navbar title="TextUtils" about="About" />
+      <div className='container my-3'>
+        <Routes>
+          <Route path="/" element={<Textform heading="Enter The Text To Analyze Below" />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
