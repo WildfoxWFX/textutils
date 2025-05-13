19d01c00fa8ccb952d22c2032c77bdf41270dcde
Prajwal <prajju.18gryphon@gmail.com>
2025-05-13 18:27:43 +0530

    Router

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import Navbar from './Components/Navbar';
 import Textform from './Components/Textform';
 import About from './Components/About';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import React, {useState} from 'react';
 import Alert from './Components/Alert';
 
@@ -27,11 +27,13 @@ function App() {
       setMode('dark');
       document.body.style.backgroundColor = '#042743';
       showAlert("Dark Mode has been Enabled","success")
+      document.title = "TextUtils - DarkMode";
     }
     else{
       setMode('light');
       document.body.style.backgroundColor = 'white';
       showAlert("Light Mode has been Enabled","success")
+      document.title = "TextUtils - LightMode";
     }
   }
 
@@ -43,7 +45,7 @@ function App() {
       <div className='container my-3'>
         <Routes>
           <Route path="/" element={<Textform showAlert={showAlert} heading="Enter The Text To Analyze Below" mode={mode} />} />
-          <Route path="/about" element={<About />} />
+          <Route path="/about" element={<About togglemode={toggleMode} about="About Us" mode={mode}/>} />
         </Routes>
       </div>
     </Router>
